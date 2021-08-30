c33ad3de80ef4eb22cd3659b9514c7617add4aca
sergekuv <sergekuv@yandex.ru>
2021-08-30 08:42:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1_Mod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
